736f5d63ea238a303f894e2a9ae27862ffc7f72e
CHHyuk <gksgur1038@naver.com>
2024-01-10 14:16:05 +0900

    메인페이지 링크 입력, 버튼 기능

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import HeroForm from "@/components/forms/Heroform";
 
 
 export default function Home() {
@@ -12,15 +13,7 @@ export default function Home() {
             Share your links, social profiles, contact info and more on one page
           </h2>
         </div>
-        <form className='inline-flex items-center shadow-lg shadow-gray-500/20'>
-          <span className='bg-white py-4 pl-4'>
-            linklist.to/
-          </span>
-          <input type="text" className='py-4' placeholder='username' />
-          <button type="submit" className='bg-blue-500 text-white py-4 px-6'>
-            Join for Free
-          </button>
-        </form>
+        <HeroForm />
       </section>
     </main>
   )
