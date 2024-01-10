ccb6606d5c2ad02f83c0885bfc6d3f783401edd6
CHHyuk <gksgur1038@naver.com>
2024-01-10 12:10:38 +0900

    버튼 활성화

--- src/components/buttons/LoginWithGoogle.js
+++ src/components/buttons/LoginWithGoogle.js
@@ -1,14 +1,18 @@
 'use client'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faGoogle } from "@fortawesome/free-brands-svg-icons"
+import { signIn, useSession } from "next-auth/react";
 
 export default function LoginWithGoogle() {
+  const data = useSession();
+  console.log(data)
+  
   return (
-  <button
-    onClick={() => {}}
-    className="bg-white shadow text-center w-full py-4 flex gap-3 justify-center items-center">
-    <FontAwesomeIcon icon={faGoogle} className="h-6" />
-    <span>Sign In with Google</span>
-  </button>
+    <button
+      onClick={() => signIn('google')}
+      className="bg-white shadow text-center w-full py-4 flex gap-3 justify-center items-center">
+      <FontAwesomeIcon icon={faGoogle} className="h-6" />
+      <span>Sign In with Google</span>
+    </button>
   );
 }
